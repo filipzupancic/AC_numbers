cbc0b8f96fbb03dc5a7dd45db80995dd7a8dedb8
filipzupancic <filipzupancic123@gmail.com>
2018-10-08 22:35:22 +0200

    Adding tests

---
+++ make_test.m
@@ -0,0 +1,24 @@
+%load('P0.mat');
+%load('P1.mat');
+%load('P2.mat');
+%load('P3.mat');
+%load('P4.mat');
+%load('P5.mat');
+%load('P6.mat');
+%load('P7.mat');
+%load('P8.mat');
+%load('P9.mat');
+%load('P10.mat');
+%load('P11.mat');
+%load('P12.mat');
+%load('P13.mat');
+%load('P14.mat');
+%load('P15.mat');
+%load('P16.mat');
+%load('P17.mat');
+%load('P18.mat');
+%load('P19.mat');
+
+P_1000 = [P0 P1 P2 P3 P4 P5 P6 P7 P8 P9 P10 P11 P12 P13 P14 P15 P16 P17 P18 P19];
+
+%save P_1000.mat P_1000
